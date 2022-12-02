a0a81c1edeae2b468534b43649a6e467f93d1564
JunyHarang <junyharang8592@gmail.com>
2022-12-02 19:09:04 +0900

    Docker 기동 명령어 Shell Script 및 프로메테우스 설정 yml File 구성

---
+++ docker/grafana/grafana-docker-build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+docker run \
+--name {Docker Container 이름 - 자유 지정} -d \
+--restart=unless-stopped \
+-p {Docker Host Port - 자유 지정}:{Garafana Container 내부 Port} \
+--ip {해당 Container 고정하고 싶은 IP 주소} \
+grafana/grafana
